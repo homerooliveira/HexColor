3e88ed15192abc88871c2ab3e67390b276144fce
homerooliveira <homero.santos.001@acad.pucrs.br>
2019-04-18 23:15:57 -0300

    Initial Commit

---
+++ HexColor/HexColor.h
@@ -0,0 +1,19 @@
+//
+//  HexColor.h
+//  HexColor
+//
+//  Created by Homero Oliveira on 18/04/19.
+//  Copyright © 2019 Homero Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexColor.
+FOUNDATION_EXPORT double HexColorVersionNumber;
+
+//! Project version string for HexColor.
+FOUNDATION_EXPORT const unsigned char HexColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexColor/PublicHeader.h>
+
+
